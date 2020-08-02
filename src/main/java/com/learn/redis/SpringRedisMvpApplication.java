e551b91ea9c2426271c6057704c8f700a65e4a52
Ruchir Agrawal <masterruchir.18@gmail.com>
2020-08-02 15:22:51 +0530

    Added Boiler Plate Code Closes #1

---
+++ src/main/java/com/learn/redis/SpringRedisMvpApplication.java
@@ -0,0 +1,13 @@
+package com.learn.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisMvpApplication.class, args);
+	}
+
+}
